d7e58a71896e4e1a05f3e3582c8f58ba2a0d510e
Roudy HAJO <roudy.hajo@etu.univ-nantes.fr>
2026-07-14 15:05:20 +0100

    agent storage budget = 40.0

--- agent/main_agent.py
+++ agent/main_agent.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
         db_user=cfg['user'],
         db_password=cfg['password'],
         db_name=cfg['dbname'],
-        storage_budget=30.0
+        storage_budget=40.0
     )
     
     try:
