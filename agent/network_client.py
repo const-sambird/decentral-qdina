6ed5e0e69bd9a2662e8c2bb2907dc963faa2cc8b
Roudy HAJO <roudy.hajo@etu.univ-nantes.fr>
2026-07-21 11:32:46 +0100

    fix end of episodes

--- agent/network_client.py
+++ agent/network_client.py
@@ -245,13 +245,25 @@ class QDinaNetworkClient:
                 
                 if response.stop_training:
                     print(f"[Worker Client {self.replica_id}] Master broadcasted stop_training signal. Resetting local environment and acknowledging...")
+
+                    active_indexes = self.env.get_active_index_names()
+
                     local_state, _ = self.env.reset()
                     self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
                     current_cost_tracker = 0.0
                     current_storage_usage = 0.0
                     costs_per_template = [0.0] * self.n_templates
 
-                    ack_metrics = qdina_pb2.LocalMetrics(..., local_reset=True)
+                    ack_metrics = qdina_pb2.LocalMetrics(
+                        replica_id=self.replica_id,
+                        total_cost=current_cost_tracker,
+                        costs=costs_per_template,
+                        storage_used=current_storage_usage,
+                        active_indexes=active_indexes,
+                        local_reset=True
+                    )
+
+
                     while True:
                         ack_response = self.stub.SubmitMetricsAndGetWorkload(ack_metrics)
                         if not ack_response.stop_training:
