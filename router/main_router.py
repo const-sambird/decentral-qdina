695b259e1f5ad467cc64e43051c1dca41621a788
Roudy HAJO <roudy.hajo@etu.univ-nantes.fr>
2026-07-22 12:04:42 +0100

    update router decay_rate

--- router/main_router.py
+++ router/main_router.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
         steps_per_episode = 100
         epsilon_start = 1.0
         epsilon_min = 0.30
-        decay_rate = 0.9999
+        decay_rate = 0.99
         
         for episode in range(args.episodes):
             print(f"\n--- [Master Orchestrator] Starting Global Episode {episode + 1}/{args.episodes} ---")
