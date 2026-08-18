7f9375562364a5650060e3bd841a664cb23a7c43
Roudy HAJO <roudy.hajo@etu.univ-nantes.fr>
2026-08-18 17:27:58 +0100

    update build_tpch_db.sh

--- build_tpch_db.sh
+++ build_tpch_db.sh
@@ -181,7 +181,11 @@ if [ "$MODE" != "benchmark" ]; then
     wait
 
     for f in *.tbl*; do
-        sed -i 's/|$//' "$f" &
+        # Supprime les espaces superflus en fin de ligne et s'assure du format de fin de ligne standard
+        sed -i 's/\r//g' "$f"
+        # S'assure que chaque ligne se termine bien par un unique délimiteur '|' si attendu par le format
+        # ou nettoie les pipes traînantes incorrects
+        sed -i 's/|[[:space:]]*$/|/' "$f" &
     done
     wait
 
